d070631aba1ef4da4015d5d9ca1e8da14661e4ca
KsenyaKoss <roxenn34@gmail.com>
2023-03-17 09:32:01 +0100

    task-1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const createElement = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+     </a>
+     </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", createElement);
+
+
+
+gallery.addEventListener("click", (ev) => {
+     ev.preventDefault();
+  if (ev.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+    <img src="${ev.target.dataset.source}" width="800" height="600">`);
+    console.log(instance);
+    instance.show();
+  } 
+});
+
+
+// function keyClose(ev) {
+//   if(instance.visible()) {
+//     if(ev.target.code === "Escape") instance.close();
+// }
+// };
+ 
+// gallery.addEventListener('keydown', keyClose)
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+const createListItems = galleryItems.map(
+    ({preview, original, description}) => `
+    <a class="gallery__item" href ="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+).join('');
+
+gallery.insertAdjacentHTML('afterbegin', createListItems);
+
+const options = {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'
+  };
+  
+const lightbox = new SimpleLightbox('.gallery a', options);
+
+gallery.addEventListener('click', (ev)=> {
+    ev.preventDefault();
+    if(ev.target.nodeName === "IMG") lightbox.on();
+})
